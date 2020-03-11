9fc3ac7129abd4ceeebfa509c4d9af6bc033e2cf
Daniel Neuweiler <11644253+xSNOWM4Nx@users.noreply.github.com>
2020-03-11 23:43:42 +0100

    Adding new indicator

---
+++ src/components/indicators/Indicator1.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+interface ILocalProps {
+  scale?: number;
+  color?: string;
+}
+type Props = ILocalProps;
+
+export const Indicator1: React.FC<Props> = (props) => {
+
+  // Helpers
+  const scale = props.scale !== undefined ? props.scale : 1.0;
+  const color = props.color !== undefined ? props.color : '#fff';
+
+  return (
+    <div
+      className='indicator1-root'
+      style={{
+        transform: `transform: scale(${scale})`,
+      }}>
+
+      <div
+        style={{
+          border: `4px solid ${color}`
+        }} />
+      <div
+        style={{
+          border: `4px solid ${color}`
+        }} />
+    </div>
+  );
+}
